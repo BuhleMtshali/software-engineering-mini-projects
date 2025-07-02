3a69f82154493c6835c59bd356f02406e533faf8
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-02 23:40:55 +0200

    updated quiz game

--- month-1-js-tools/day-02-js-variables/quizGame.js
+++ month-1-js-tools/day-02-js-variables/quizGame.js
@@ -1,3 +1,14 @@
-let answer;
-const welcomeMessage = '====Welcome to MiniQuiz Game👾===';
+const prompt = require("prompt-sync")();
+
+const answer = "cape town";
+const welcomeMessage = '==== Welcome to MiniQuiz Game👾 ====';
 console.log(welcomeMessage);
+
+let question = prompt("What City is referred to as Mother City?: ");
+
+while(question.toLowerCase() !== answer.toLowerCase()) {
+    console.log(`${question} is incorrect. Try again!`);
+    question = prompt("What City is referred to as Mother City?: ");
+}
+
+console.log(`${question} is correct!! 🥳🎉`);
