5c1c7adb9d7d96ab73e654c8b85e59e052b15a7d
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-03 20:10:53 +0200

    updated the loops file

--- month-1-js-tools/day-03-loops-functions/loops-js.js
+++ month-1-js-tools/day-03-loops-functions/loops-js.js
@@ -1,7 +1,7 @@
 const prompt = require("prompt-sync")();
 
 let userIterator = parseFloat(prompt("Enter the value of your iterator: "));
-
+console.log(`---- You chose ${userIterator} as your maximum iterator, here is ur mini FizzBuzz Game ----`)
 //get the the value of i from the user
 for(let i = 1; i <= userIterator; i++ ){
     if(i % 3 === 0 || i % 5 === 0){
