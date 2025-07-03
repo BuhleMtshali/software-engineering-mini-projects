d6f16aa6c904a6133d649652e97cf0a0412c431e
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-03 20:16:15 +0200

    updated the for loop function

--- month-1-js-tools/day-03-loops-functions/loops-js.js
+++ month-1-js-tools/day-03-loops-functions/loops-js.js
@@ -1,10 +1,16 @@
 const prompt = require("prompt-sync")();
 
+console.log("=== Welcome to your Mini FizzBuzz Game ===");
+
+console.log(" 🍀you actually get to choose your max iterator value🍀");
+
 let userIterator = parseFloat(prompt("Enter the value of your iterator: "));
+
 console.log(`---- You chose ${userIterator} as your maximum iterator, here is ur mini FizzBuzz Game ----`)
+
 //get the the value of i from the user
 for(let i = 1; i <= userIterator; i++ ){
-    if(i % 3 === 0 || i % 5 === 0){
+    if(i % 3 === 0 && i % 5 === 0){
         console.log("Fizz!!!!!")
     } else if(i % 3 === 0){
         console.log("Fizz!!!")
