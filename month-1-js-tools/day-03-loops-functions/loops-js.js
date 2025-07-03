efe454355f1dd1369f7b6759376dcb996e2589e3
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-03 20:03:30 +0200

    updated the loops value

---
+++ month-1-js-tools/day-03-loops-functions/loops-js.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+
+let userIterator = int(prompt("Enter the value of your iterator: "));
+
+//get the the value of i from the user
+for(let i = 1; i <= userIterator; i++ ){
+    if(i % 3 === 0 || i % 5 === 0){
+        console.log("Fizz!!!!!")
+    } else if(i % 3 === 0){
+        console.log("Fizz!!!")
+    } else if(i % 5 === 0){
+        console.log("Fizz!!!")
+    } else{
+        console.log(i)
+    }
+}
