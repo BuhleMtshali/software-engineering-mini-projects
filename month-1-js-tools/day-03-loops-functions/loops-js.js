95fcdc5983b02c1c1dbb0f655f7abf18dfa1403f
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-04 19:26:36 +0200

    updated the loops logic

--- month-1-js-tools/day-03-loops-functions/loops-js.js
+++ month-1-js-tools/day-03-loops-functions/loops-js.js
@@ -12,11 +12,11 @@ console.log(`---- You chose ${userIterator} as your maximum iterator, here is ur
 //get the the value of i from the user
 for(let i = 1; i <= userIterator; i++ ){
     if(i % 3 === 0 && i % 5 === 0){
-        console.log("Fizz!!!!!")
+        console.log("FizzBuzz!!!!!")
     } else if(i % 3 === 0){
-        console.log("Fizz!!!")
+        console.log("FizzBuzz!!!")
     } else if(i % 5 === 0){
-        console.log("Fizz!!!")
+        console.log("FizzBuzz!!!")
     } else{
         console.log(i)
     }
