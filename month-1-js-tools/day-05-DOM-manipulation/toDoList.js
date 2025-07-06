0c5e7bd4e0b2e77de00a4f149ffe6cd91483399a
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-06 19:46:43 +0200

    updated the js file

--- month-1-js-tools/day-05-DOM-manipulation/toDoList.js
+++ month-1-js-tools/day-05-DOM-manipulation/toDoList.js
@@ -1,3 +1,15 @@
 //defining the variables
 const inputBox = document.querySelector('#search-bar');
-const addTaskBtn = document.querySelector('#add-task')
+const addTaskBtn = document.querySelector('#add-task');
+let deleteBtn = document.querySelectorAll('.delete-btn');
+
+//todo list to store tasks
+const taskList = [];
+
+addTaskBtn.addEventListener('click', () => {
+   if(!inputBox.value){
+    alert('Please add a task first')
+   } else {
+    
+   }
+})
