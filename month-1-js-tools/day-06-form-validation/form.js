3bbe654fa2fae5a0ef4e3f5c5e3e04f26d092b45
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-06 21:11:08 +0200

    updated the forms

--- month-1-js-tools/day-06-form-validation/form.js
+++ month-1-js-tools/day-06-form-validation/form.js
@@ -1,4 +1,12 @@
-const name = document.querySelector('#name');
+const userName = document.querySelector('#name');
 const lastName = document.querySelector('#lastName');
 const email = document.querySelector('#email');
+const feedback = document.querySelector('#textarea');
+const formWrapper = document.querySelector('#form-wrapper')
 
+
+formWrapper.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('form submitted');
+    
+})
