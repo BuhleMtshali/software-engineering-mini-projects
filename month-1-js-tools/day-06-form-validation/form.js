26f78c9caa7d8d57acba0b8ccb951653364aca3c
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-06 21:44:55 +0200

    updated the index folder

--- month-1-js-tools/day-06-form-validation/form.js
+++ month-1-js-tools/day-06-form-validation/form.js
@@ -2,22 +2,23 @@ const userName = document.querySelector('#name');
 const lastName = document.querySelector('#lastName');
 const email = document.querySelector('#email');
 const feedback = document.querySelector('#textarea');
-const formWrapper = document.querySelector('#form-wrapper')
-
+const formWrapper = document.querySelector('#form-wrapper');
+const innerWrapper = document.querySelector('.inner-wrapper');
+const submitted = document.querySelector('.submitted-form')
+const submittedForm = document.querySelector('.submitted-form');
 
 formWrapper.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log('form submitted');
-    formWrapper.innerHTML = `
-                            <div class="submitted-form">
-                        <h1>Form Successfully Submitted!!!</h1>
-                        <p>Thank you for submitting your feedback!</p>
-                        <button id="reset-form">Reset Form</button>
-                        </div>  
-                  `
-    let resetBtn = document.querySelector('#reset-form');
-resetBtn.addEventListener('click', () => {
-    formWrapper.reset()
-})
-})
+  e.preventDefault();
+  console.log('form submitted');
 
+  formWrapper.style.display = 'none';
+  submittedForm.style.display = 'flex';
+});
+
+const resetBtn = document.querySelector('#reset-form');
+
+resetBtn.addEventListener('click', () => {
+  formWrapper.reset(); // resets all fields
+  formWrapper.style.display = 'flex';
+  submittedForm.style.display = 'none';
+});
