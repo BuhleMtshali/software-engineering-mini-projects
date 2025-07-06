9c1227558bfc57b0ddcc5848c6b37356b59d5dea
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-06 21:25:46 +0200

    updated the app.css and form.js

--- month-1-js-tools/day-06-form-validation/form.js
+++ month-1-js-tools/day-06-form-validation/form.js
@@ -8,5 +8,16 @@ const formWrapper = document.querySelector('#form-wrapper')
 formWrapper.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log('form submitted');
-    
+    formWrapper.innerHTML = `
+                            <div class="submitted-form">
+                        <h1>Form Successfully Submitted!!!</h1>
+                        <p>Thank you for submitting your feedback!</p>
+                        <button id="reset-form">Reset Form</button>
+                        </div>  
+                  `
+    let resetBtn = document.querySelector('#reset-form');
+resetBtn.addEventListener('click', () => {
+    formWrapper.reset()
 })
+})
+
