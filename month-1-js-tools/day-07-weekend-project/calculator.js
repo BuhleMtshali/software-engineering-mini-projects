c3be5b79baf8294179cb824becd2de8ebb4c11b3
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-08 20:48:21 +0200

    updated the calculato

--- month-1-js-tools/day-07-weekend-project/calculator.js
+++ month-1-js-tools/day-07-weekend-project/calculator.js
@@ -7,4 +7,28 @@ let operator = prompt("Choose an operator (+, -, *, /): ")
 let secondNumber = parseFloat(prompt("Enter your second number: "))
 
 let result = parseFloat(0);
+
+
+//switch to start the program
+switch(operator){
+    case '+':
+        result = firstNumber + secondNumber;
+        break;
+    case '-':
+        result = firstNumber - secondNumber;
+        break;
+    case '*':
+        result = firstNumber * secondNumber;
+        break;
+    case '/':
+        if(secondNumber != 0){
+            result = firstNumber / secondNumber
+        } else{
+            console.log('Sorry poonkie, we cannot devide by 0')
+        }
+        break;
+    default:
+        console.log(`${operator} is invalid please try again!`)
+}
+
 console.log(result)
