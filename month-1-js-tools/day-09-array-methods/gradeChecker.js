a882b384bb45b7c5280153c51c87c9e5dcf59298
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-11 17:53:17 +0200

    updated the gradechecker

--- month-1-js-tools/day-09-array-methods/gradeChecker.js
+++ month-1-js-tools/day-09-array-methods/gradeChecker.js
@@ -12,15 +12,15 @@ let checkAgain = 'yes';
 //start the while loop for the whole function
 while(checkAgain.toLowerCase() === "yes" || checkAgain.toLowerCase() === "y"){
     console.log('------- Enter your Details Below⬇️ ---------')
-    const studentName = prompt("Enter your First Name: ")
-    const lastName = prompt("Enter your last Name: ");
-    const studentNumber = prompt("Enter your 9-digit Student Number: ");
-    let studentEmail = prompt("Enter your email address: ");
+    let studentName = prompt("Enter your First Name: ")
+    let lastName = prompt("Enter your last Name: ");
+    let studentNumber = prompt("Enter your 9-digit Student Number: ").trim();
+    let studentEmail = prompt("Enter your email address: ").trim();
 
     //regex Triggers the while loop if it's NOT valid, keep looping until it's exactly 9 digits and numeric
     while(!/^\d{9}$/.test(studentNumber)){                   //This regex checks if the input has exactly 9 digits, nothing more, nothing less
         console.log('Invalid input. Please enter exactly 9 digits');
-        studentNumber = prompt("Try again. Enter your 9-digit Student Number: ");
+        studentNumber = prompt("Try again. Enter your 9-digit Student Number: ").trim();
     }
 
     // basic pattern: something@something.something for email
