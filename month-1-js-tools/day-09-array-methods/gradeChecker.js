99647bb3a9e06890e2aaddaa3a6354a53af45b54
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-11 17:28:45 +0200

    updated the student grade checker

--- month-1-js-tools/day-09-array-methods/gradeChecker.js
+++ month-1-js-tools/day-09-array-methods/gradeChecker.js
@@ -1,1 +1,21 @@
 const prompt = require("prompt-sync")();
+
+//welcome message
+const welcomeMessage = '========== Welcome to The Student Grade Checker📑 ========='
+
+//array method to store student details
+let studentDetails = [];
+
+//give the user the option of choosing to continue or not 
+let checkAgain = 'yes';
+
+//start the while loop for the whole function
+while(checkAgain.toLowerCase() === "yes" || checkAgain.toLowerCase() === "y"){
+    console.log('------- Enter your Details Below⬇️ ---------')
+    const studentName = prompt("Enter your First Name: ")
+    const lastName = prompt("Enter your last Name: ");
+    const studentNumber = prompt("Enter your 9-digit Student Number: ");
+    while(studentNumber.length === 9){
+       break;
+    }
+}
