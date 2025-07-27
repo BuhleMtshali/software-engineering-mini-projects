7e753e75c4ff5c19f7d7154e48e0d65adbad09eb
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-27 17:52:03 +0200

    updated the fizzbuz game

--- month-1-js-tools/Day-03-loops-functions/fizzbuzWhileLoop.js
+++ month-1-js-tools/Day-03-loops-functions/fizzbuzWhileLoop.js
@@ -1,13 +1,14 @@
 const prompt = require('prompt-sync')();
 
 function miniFizzbuzz(){
+    let keepPlaying = "yes";
+    while(keepPlaying.toLocaleLowerCase() === "yes"){
+        let number = parseFloat(prompt('Choose an iterator: '));
+        let iterator = 0;
 
-    let number = parseFloat(prompt('Choose an iterator: '));
-    let iterator = 0;
-
-    if(isNaN(number)){
-        console.log('The chosen max iterator value is incorrect!!!')
-    } else {
+        if(isNaN(number)){
+            console.log('The chosen max iterator value is incorrect!!!')
+        } else {
             while( iterator <= number){
             if(iterator % 3 === 0 && iterator % 5 === 0){
                 console.log('FizzBuzz🎉')
@@ -21,7 +22,13 @@ function miniFizzbuzz(){
             iterator++;
         }
 
-    }
+        }
+        keepPlaying = prompt("Wanna go another round(yes/no): ")
+        if(keepPlaying.toLocaleLowerCase() === 'no'){
+            console.log('thank you for trying my mini game')
+        }
+      }
+    
 
 }
 
