c46c8a3f93c610d3a2a40ab86f05b957559f94f2
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-28 18:34:23 +0200

    updated the inventory list

---
+++ month-1-js-tools/Day-04-arrays-objects/inventoryTracker.js
@@ -0,0 +1,23 @@
+const prompt = require('prompt-sync')();
+
+//first defining the array that will hold the whole inventory
+let inventoryArray = [];
+
+let welcomeMessage = "======== Welcome To My Mini Inventory Tracker🛒 =======";
+console.log(welcomeMessage)
+
+//main function
+function inventoryFunction(){
+    while(true){
+        //user options
+        console.log("Please choose an Option Below: \n");
+        console.log("1. View Inventory List: ")
+        console.log("2. Add Item to Inventory: ")
+        console.log("3. Remove an Item from Inventory: ")
+
+
+
+
+    }
+
+}
