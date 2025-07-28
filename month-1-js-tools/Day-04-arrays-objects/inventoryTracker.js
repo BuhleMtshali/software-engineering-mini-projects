6ea1be56837df64e545f31f010ec94c4cd31f56e
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-28 20:25:26 +0200

    updated the inventorytracker app

--- month-1-js-tools/Day-04-arrays-objects/inventoryTracker.js
+++ month-1-js-tools/Day-04-arrays-objects/inventoryTracker.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')();
 
 //first defining the array that will hold the whole inventory
-let inventoryArray = [{name: 'buhle'}];
+let inventoryArray = [];
 
 let welcomeMessage = "======== Welcome To My Mini Inventory Tracker🛒 =======";
 console.log(welcomeMessage)
@@ -37,18 +37,18 @@ function inventoryFunction(){
 
             //THIS IS FOR THE SECOND Case 2. Adding item to inventory
             case "2":
-                let productName = prompt("Enter the product name: ");
-                let productCategory = prompt("Enter the product catergory: ");
-                let productPriceEach = parseFloat(prompt("How much is the product each in (R): "));
-                let productQuantity = parseInt(prompt("How many products are you adding: "));
-                let total = productQuantity * productPriceEach
+                let pName = prompt("Enter the product name: ");
+                let pCatergory = prompt("Enter the product catergory: ");
+                let pPriceEach = parseFloat(prompt("How much is the product each in (R): "));
+                let pQuantity = parseInt(prompt("How many products are you adding: "));
+                let total = pQuantity * pPriceEach
 
                //creating the product object
                 let product = {
-                    pName: productName.trim(),
-                    pCatergory: productCategory.trim(),
-                    pPriceEach: productPriceEach.trim(),
-                    pQuantity: productQuantity.trim(),
+                    productName: pName.toLowerCase().trim(),
+                    productCategory: pCatergory.trim(),
+                    productPriceEach: pPriceEach,
+                    productQuantity: pQuantity,
                     overallPrice: total.toFixed(2)
                 }
 
@@ -56,9 +56,51 @@ function inventoryFunction(){
                 inventoryArray.push(product);
 
                 //confirm its been added
-                console.log(`✅ Your ${productName} has been successfully added to the inventory list!`);
+                console.log(`✅ Your ${pName} has been successfully added to the inventory list!`);
                 break;
-                
+
+            //case 3: remove an item based on the nme and how much you wanna remove
+            case "3":
+                let removeItem = prompt("Enter the name of the product you want to remove: ").toLowerCase();
+
+                //finding the item we want to remove from
+                const foundItem = inventoryArray.find(item => item.productName === removeItem);
+
+                //using if statements to remove the itesms
+                if(foundItem){
+
+                    //here is the logic to execute if the item is found
+                    console.log(foundItem);
+                    let quantityToRemove = parseInt(prompt(`You currently have ${foundItem.productQuantity} ${foundItem.productName}'s, how many would you like to remove: `))
+                    if (foundItem.productQuantity >= quantityToRemove){
+                        foundItem.productQuantity = foundItem.productQuantity - quantityToRemove;
+                        console.log(`✅ We have successfuly removed ${quantityToRemove} ${foundItem.productName}'s from inventory, you now have ${foundItem.productQuantity} ${foundItem.productName}'s in your inventory`)
+                        console.log(foundItem)
+
+                    } else if(foundItem.productQuantity < quantityToRemove){
+                        console.log(`❌ You only have ${foundItem.productQuantity} ${foundItem.productName}'s in your inventory, so ${quantityToRemove} is not valid in this case`)
+
+                    } else {
+                        console.log(`🚫 ${quantityToRemove} is not valid in this case, try another number`)
+                    }
+                    //end of found logic
+
+                } else {
+                    console.log(`${removeItem} does not currently exist in your inventory list!`)
+                }
+                break;
+            
+            //case "4": searching for items based on catergory
+            case "4":
+                let searchedItem = prompt("Would you like to search items based on the name/catergory: ")
+                let nameOfItem = prompt("Enter the name/catergory of the item you want to search for: ")
+
+                //decide what to show based on what the user returns
+                if(searchedItem.toLowerCase() === "name"){
+                    const searchByName = inventoryArray.filter(item => item.productName === s)
+                }
+
+
 
 
             default:
