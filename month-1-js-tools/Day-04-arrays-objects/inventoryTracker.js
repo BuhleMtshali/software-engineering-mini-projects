efc26c32e4a6550ae1178a00da017c476ab15109
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-28 18:41:00 +0200

    updated the inventory tracker

--- month-1-js-tools/Day-04-arrays-objects/inventoryTracker.js
+++ month-1-js-tools/Day-04-arrays-objects/inventoryTracker.js
@@ -9,15 +9,34 @@ console.log(welcomeMessage)
 //main function
 function inventoryFunction(){
     while(true){
+
         //user options
         console.log("Please choose an Option Below: \n");
         console.log("1. View Inventory List: ")
         console.log("2. Add Item to Inventory: ")
         console.log("3. Remove an Item from Inventory: ")
+        console.log("4. Search By Catergory: ")
+        console.log("5. Exit")
+
+        //get the user option
+        let options = prompt("Please select an option to continue: ");
+
+        //start the switch statements
+        switch(options){
+            case "1":
+                console.log('i work')
+                break
 
 
+            default:
+                console.log('this is the default block');
+                break
+
+        }
 
 
     }
 
 }
+
+inventoryFunction()
