7f7593dfb10590b9ac91783c34ebef8ef5cf191e
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-08 07:20:19 +0200

    updated the methods

--- month-1-js-tools/Day-07-weekend-project/basicCalculator.js
+++ month-1-js-tools/Day-07-weekend-project/basicCalculator.js
@@ -6,23 +6,23 @@ class Calculator{
         this.num2 = num2;
     }
 
-    addMethod(){
-        return this.num1 + this.num2;
+    addMethod(num1, num2){
+        return num1 + num2;
     }
 
-    minusMethod(){
-        return this.num1 - this.num2;
+    minusMethod(num1, num2){
+        return num1 - num2;
     }
 
-    multiplyMethod(){
-        return this.num1 * this.num2;
+    multiplyMethod(num1, num2){
+        return num1 * num2;
     }
 
-    divideMethod(){
-        if (this.num2 === 0){
+    divideMethod(num1, num2){
+        if (num2 === 0){
             console.log("we cannot divide by 0")
         } else {
-            return this.num1 / this.num2;
+            return num1 / num2;
         }
     }
 }
@@ -32,8 +32,9 @@ let output = 0;
 function mainCalculator(){
     const myCalculator = new Calculator();
 
-    output = myCalculator.addMethod(4, 5)
-    console.log(output)
+    //output = myCalculator.addMethod(4, 5)
+    console.log(myCalculator.addMethod(4, 5))
+    console.log(myCalculator.multiplyMethod(3, 9))
 }
 
 
