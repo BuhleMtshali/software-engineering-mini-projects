613c7499a57e3520688cf45835717233b03515ea
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-08 07:04:57 +0200

    created a basic class strtucture for the calcular

--- month-1-js-tools/Day-07-weekend-project/basicCalculator.js
+++ month-1-js-tools/Day-07-weekend-project/basicCalculator.js
@@ -5,4 +5,28 @@ class Calculator{
         this.num1 = num1;
         this.num2 = num2;
     }
+
+    addMethod(){
+        return this.num1 + this.num2;
+    }
+
+    minusMethod(){
+        return this.num1 - this.num2;
+    }
+
+    multiplyMethod(){
+        return this.num1 * this.num2;
+    }
+
+    divideMethod(){
+        if (this.num2 === 0){
+            console.log("we cannot divide by 0")
+        } else {
+            return this.num1 / this.num2;
+        }
+    }
 }
+
+
+const myCalculator = new Calculator();
+
