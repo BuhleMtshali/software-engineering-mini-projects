aa370392a82360157d2b12ed37a28953a4780e4a
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-14 19:56:58 +0200

    updated the grade analyzer

--- month-1-js-tools/Day-09-Array-Methods/gradeAnalyzer.js
+++ month-1-js-tools/Day-09-Array-Methods/gradeAnalyzer.js
@@ -5,24 +5,37 @@ const gradeArray = [];
 
 
 class Student{
-    constructor(name, studentNumber, grade){
+    constructor(name, studentNumber, module, grade){
         this.name = name
-        this.studentNumber = studentNumber
+        this.studentNumber = studentNumber,
+        this.module = module,
         this.grade = grade
     }
 
     addStudent(){
-       let studentWrapper = {
+        if(this.name.length() > 0 && Number.isInteger(this.grade)){
+            let studentWrapper = {
             studentName: this.name,
             idNumber: this.studentNumber,
+            studentModule: this.module,
             studentGrade: this.grade
         }
-        gradeArray.push(studentWrapper);
-        console.log(`✅ student succeeffuly logged`)
+            gradeArray.push(studentWrapper);
+            console.log(`✅ student succeeffuly logged`)
+        } else {
+            console.log("🚫 Student cannot be added")
+        }
+       
     }
 }
 
 function logStudent(){
+    let studentName = prompt("Enter your name: ");
+    let studentNumber = parseFloat(prompt("Enter your student number: "));
+    let studentGrade = parseFloat(prompt("Enter your grade: "));
+
+
+
     const student = new Student('Zano', 9823423, 76);
     student.addStudent();
     console.log(gradeArray);
