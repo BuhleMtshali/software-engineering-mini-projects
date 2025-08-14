07c9ca512c0ba21df148acf656070690c9f6a78f
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-14 19:23:09 +0200

    created the class for the grade analyzer

--- month-1-js-tools/Day-09-Array-Methods/gradeAnalyzer.js
+++ month-1-js-tools/Day-09-Array-Methods/gradeAnalyzer.js
@@ -1,3 +1,13 @@
 const prompt = require("prompt-sync")();
 
 //CREATING AN ARRAY TO HOLD THE GRADES
+const gradeArray = [];
+
+
+class Student{
+    constructor(name, studentNumber, grade){
+        this.name = name
+        this.studentNumber = studentNumber
+        this.grade = grade
+    }
+}
