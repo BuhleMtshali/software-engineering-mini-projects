886376db6418807fab7cb6ddc0e96a8d502fcabc
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-26 14:18:14 +0200

    updated the bank account class

--- month-1-js-tools/Day-10-object-methods/bankAccount.js
+++ month-1-js-tools/Day-10-object-methods/bankAccount.js
@@ -1,8 +1,10 @@
 //CREATING CLASS FOR BANK
 class Bank{
-    constructor(name, accountNumber, balance){
+    constructor(name, accountNumber, pin, accountType, balance){
         this.name = name;
         this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.pin = pin;
         this.balance = balance
     }
 }
