1062d5f008cb3e82285d45e3133bc8098704099d
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-26 14:16:05 +0200

    created the bank account class

---
+++ month-1-js-tools/Day-10-object-methods/bankAccount.js
@@ -0,0 +1,8 @@
+//CREATING CLASS FOR BANK
+class Bank{
+    constructor(name, accountNumber, balance){
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance
+    }
+}
