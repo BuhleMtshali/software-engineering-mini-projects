11739dda317c45d24f94bf6cbcb0e0f9460a0943
Buhle <mtshalizanobuhle7@gmail.com>
2025-09-03 15:37:25 +0200

    fixed the modules

--- month-1-js-tools/Day-11-APIs/urlScanner.js
+++ month-1-js-tools/Day-11-APIs/urlScanner.js
@@ -1,6 +1,29 @@
-require('dotenv').config();
-const prompt = require("prompt-sync")();
+import dotnev from "dotenv";
+import PromptSync from "prompt-sync";
 import axios from 'axios';
+
+dotnev.config() //this loads the env file
+
+const prompt = PromptSync();
 
 const apiKey = process.env.API_KEY;
 console.log('Apikey_length', apiKey?.length)
+
+const options = {
+  method: 'GET',
+  url: 'https://malicious-scanner.p.rapidapi.com/rapid/url',
+  params: {
+    url: 'https://vryjm.page.link/jS6a'
+  },
+  headers: {
+    'x-rapidapi-key': apiKey,
+    'x-rapidapi-host': 'malicious-scanner.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
