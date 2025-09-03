a5ba97ac64914763ab84154e944660654e607bce
Buhle <mtshalizanobuhle7@gmail.com>
2025-09-03 15:31:08 +0200

    added axios

--- month-1-js-tools/Day-11-APIs/urlScanner.js
+++ month-1-js-tools/Day-11-APIs/urlScanner.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const prompt = require("prompt-sync")();
+import axios from 'axios';
 
 const apiKey = process.env.API_KEY;
 console.log('Apikey_length', apiKey?.length)
