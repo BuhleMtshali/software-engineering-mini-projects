52e68fb50385c4905eda59f18f67a74f274f5399
Buhle <mtshalizanobuhle7@gmail.com>
2025-09-07 19:31:16 +0200

    added the search icon

--- month-1-js-tools/Day-12-recipeFinder/app.js
+++ month-1-js-tools/Day-12-recipeFinder/app.js
@@ -4,3 +4,6 @@ const midNav = document.querySelector(".mid-nav");
 hamburger.addEventListener("click", () => {
   midNav.classList.toggle("active");
 });
+
+//altering the body
+let bodyWrapper = document.querySelector(".recipe-wrapper");
