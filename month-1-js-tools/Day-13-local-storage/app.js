a78099cb189e9497afa6bcd5b55e545a5974e03e
Buhle <mtshalizanobuhle7@gmail.com>
2025-09-15 15:05:08 +0200

    updated the index file

--- month-1-js-tools/Day-13-local-storage/app.js
+++ month-1-js-tools/Day-13-local-storage/app.js
@@ -21,7 +21,25 @@ window.addEventListener("click", (event) => {
 // SETTING UP THE LOGIN PAGE
 let loginForm = document.querySelector("#login")
 let userName = document.querySelector("#username").value.trim()
+
 if (userName){
+
     //THIS IS WHERE I SAVE TO LOCAL STORAGE
     localStorage.setItem("username", userName);
+
+    //THIS REDIRECTS TO THE BOOKLIST.HTML
+    window.location.href = "booklist.html";
+} else {
+    alert("🚫 Please enter your name!")
+}
+
+
+// GETTING THE USERNAME
+let loggedInUser = localStorage.getItem("username")
+
+if (!userName){
+    //IF THERE ARE NO USERS WE SEND THEM BACK TO THE INDEX FILE
+    window.location.href = "index.html";
+} else {
+    document.querySelector("#user").textContent = userName
 }
