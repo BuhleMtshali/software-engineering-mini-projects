2a6d7c42884269e513a6795ac9af482f67847ade
Buhle <mtshalizanobuhle7@gmail.com>
2025-10-11 18:32:04 +0200

    updated the js logic for the modal

--- month-1-js-tools/Day-13-local-storage/app.js
+++ month-1-js-tools/Day-13-local-storage/app.js
@@ -1,5 +1,7 @@
-//DEFINING VARIABLES
+// SETTING UP THE MODAL
+const modal = document.querySelector("#modal");
+const closeModal = document.querySelector("#closeModal")
 const addNoteBtn = document.querySelector("#add-note-btn");
 addNoteBtn.addEventListener("click", () => {
-    console.log("I was clicked")
+    console.log("I was clicked!")
 })
